c22cfe912e1a44aeebdbfc1f20d1d98fa8d8b048
Akhil P L <akhilplx@gmail.com>
2023-08-17 15:29:48 +0530

    update auth functions

--- auth/functions.py
+++ auth/functions.py
@@ -1,3 +1,6 @@
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
